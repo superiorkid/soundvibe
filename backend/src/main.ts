88f54296c3fef210387427701e37c4b3b4161599
superiorkid <mohammad.ilhamuddin@gmail.com>
2025-08-14 22:17:23 +0800

    prisma init

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { appConfig } from './config/app.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: false,
+  });
   await appConfig(app);
 }
 bootstrap();
