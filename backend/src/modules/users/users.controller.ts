be6410ed0482f7fd0335b25616702afc515bcd7f
superiorkid <mohammad.ilhamuddin@gmail.com>
2025-08-17 20:23:51 +0800

    remove unused route

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,14 +1,8 @@
-import { Public, Session, type UserSession } from '@mguay/nestjs-better-auth';
+import { Public } from '@mguay/nestjs-better-auth';
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get('me')
-  getProfile(@Session() session: UserSession) {
-    const user = session.user;
-    return { data: user, success: true, message: 'get session successfully' };
-  }
-
   @Public()
   @Get('public')
   pulicRuote() {
